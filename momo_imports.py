e4139eff0152428d33e448fd439d58051d3e8a02
Moses Soh <moses.soh@gmail.com>
2017-12-07 11:49:06 +0800

    momo_imports.py and momo_utils.py

--- momo_imports.py
+++ momo_imports.py
@@ -1,18 +1,21 @@
 import json, os, pickle, random, math
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
 from torch.autograd import Variable
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms, utils
-from torch.utils.data import Dataset,DataLoader
-from torch.utils.data.sampler import Sampler, SubsetRandomSampler
 import torch.nn.functional as F
-import matplotlib
-import matplotlib.pyplot as plt
 from PIL import Image, ImageEnhance
-import pandas as pd
-from multiprocessing import Pool
-from tqdm import tqdm
+
+prod = True
+if not prod:
+    from torch.optim import lr_scheduler
+    import torch.optim as optim
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    from multiprocessing import Pool
+    from tqdm import tqdm
+    from torch.utils.data import Dataset,DataLoader
+    from torch.utils.data.sampler import Sampler, SubsetRandomSampler
